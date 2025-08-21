3d1754f39c04061acd8baf4631229c4ece470b16
Jonathan David Rosenblatt <jrosenblatt@fi.uba.ar>
2025-08-20 22:32:16 -0300

    Agrego generador de compose basico para cliente

--- generador.py
+++ generador.py
@@ -1,4 +1,27 @@
 import argparse
+import yaml
+
+def generar_compose(nombre_archivo, cantidad_clientes):
+    data = {
+        'version': '1',
+        'name': 'multiclient',
+        'services': {
+            f'cliente{i}': {
+                'container_name': f'cliente{i}',
+                'image': 'client:latest',
+                'entrypoint': '/client',
+                'environment': [
+                    'CLI_ID=1',
+                    'CLI_LOG_LEVEL=DEBUG'
+                ],
+                'networks': 'testing_net',
+                'depends_on': 'server',
+            }
+            for i in range(1, int(cantidad_clientes) + 1)
+        },
+    }
+    with open(nombre_archivo, "w", encoding="utf-8") as f:
+        yaml.safe_dump(data, f, sort_keys=False, allow_unicode=True)
 
 def main():
     parser = argparse.ArgumentParser(description="Generador Docker Compose")
@@ -8,6 +31,7 @@ def main():
     args = parser.parse_args()
 
     print(f"Generando Compose: '{args.nombre}' con {args.cantidad_clientes} clientes")
+    generar_compose(args.nombre, args.cantidad_clientes)
 
 if __name__ == "__main__":
     main()
