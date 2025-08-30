24bcde26ea0c8aea1ca8270aba3a975fce8fe29c
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2025-08-30 16:52:05 -0300

    Adapto script para linux

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -12,7 +12,7 @@ fi
 
 RESPUESTA=$(docker run --rm --network=tp0_testing_net busybox sh -c "echo '$MENSAJE' | nc server $SERVER_PORT")
 
-if [ "$RESPUESTA" == "$MENSAJE" ]; then
+if [ "$RESPUESTA" = "$MENSAJE" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
